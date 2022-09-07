96ea134fc9a5557e8127e6b8b69dcd6ffd5f27a6
CodeOki <davidweng12345@gmail.com>
2022-09-07 23:06:58 +0000

    This is just a 'hello world' C++ programming file. It is for CISC 3142 Exercise 1 Task 2. Created by David Weng(Student)

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+printf("Hello World
+\
+n");
+}
+#include <iostream> //preprocessor directive.
+iostream
+is the
+input
+output class in C++
+// main entry point
+int main() {
+// write a message to console
+std::cout << "Hello world!
+\
+n";
+return 0;
+}
